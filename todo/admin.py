c1bdd0c6e0f6a7f2e247e8b821ac1f209b7836ed
KRyan23 <GitHub@kryan23.bulc.club>
2022-01-23 18:59:22 +0000

    added in items functionality

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Item
 
+# from the current directories models file import the 'Item' class
 # Register your models here.
+admin.site.register(Item)
+#registers the Item model
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model): 
+    # ^ this inherits the models class so it can be used
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+#Overides the default inherited class that wou;d notmally display the oject id and 
+#instead returns the name of our created item we added in the admin panel.
+
+    def __str__(self):
+        return self.name
